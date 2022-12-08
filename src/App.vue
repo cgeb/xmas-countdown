a065d8ae32872aa1ca12569ab7d51abd69dc6b18
cgeb <ckg61386@gmail.com>
2022-12-08 15:00:53 +0900

    Solve Day 3

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,17 @@
-<script setup>
-import CountdownHeader from '@/components/CountdownHeader.vue'
+<script setup lang="ts">
+import CountdownHeader from './components/CountdownHeader.vue'
 import CountdownSegment from './components/CountdownSegment.vue'
 import { useNow } from '@vueuse/core'
+import { computed } from '@vue/reactivity'
 
 const now = useNow()
 const christmas = new Date('12/25/2022 00:00:00')
+
+const timeUntilChristmas = computed(() => christmas.getTime() - now.value.getTime())
+const seconds = computed(() => Math.floor(timeUntilChristmas.value / 1000) % 60)
+const minutes = computed(() => Math.floor((timeUntilChristmas.value / 1000 / 60) % 60))
+const hours = computed(() => Math.floor((timeUntilChristmas.value / 1000 / 60 / 60) % 24))
+const days = computed(() => Math.floor(timeUntilChristmas.value / 1000 / 60 / 60 / 24))
 </script>
 <template>
   <div class="w-full h-full flex justify-center items-center p-10">
@@ -12,10 +19,10 @@ const christmas = new Date('12/25/2022 00:00:00')
       <div class="shadow-md relative bg-white p-5 rounded-lg border-gray-100 border-[1px]">
         <CountdownHeader />
         <main class="flex justify-center">
-          <CountdownSegment label="days" number="00" />
-          <CountdownSegment label="hours" number="00" />
-          <CountdownSegment label="minutes" number="00" />
-          <CountdownSegment label="seconds" number="00" />
+          <CountdownSegment label="days" :number="days" />
+          <CountdownSegment label="hours" :number="hours" />
+          <CountdownSegment label="minutes" :number="minutes" />
+          <CountdownSegment label="seconds" :number="seconds" />
         </main>
       </div>
       <h4 class="mt-10 text-gray-400 text-center text-sm">
--- src/components/CountdownSegment.vue
+++ src/components/CountdownSegment.vue
@@ -7,7 +7,9 @@ defineProps({
 <template>
   <div class="segment text-center">
     <div class="pt-10 overflow-hidden relative">
-      <span :key="number" class="numbers text-green absolute top-0 left-[50%]">{{ number }}</span>
+      <Transition>
+        <span :key="number" class="numbers text-green absolute top-0 left-[50%]">{{ number }}</span>
+      </Transition>
     </div>
 
     <span class="label block pt-2">{{ label }}</span>
@@ -23,5 +25,24 @@ defineProps({
 }
 .label {
   font-size: 16px;
+}
+
+.v-enter-active,
+.v-leave-active {
+  transition: all 0.5s ease;
+  transform: translateY(0);
+}
+
+.v-enter-from {
+  transform: translate(-50%, -100%);
+}
+
+.v-leave-to {
+  transform: translate(-50%, 100%);
+}
+
+.v-enter-to,
+.v-leave-from {
+  transform: translate(-50%, 0);
 }
 </style>
